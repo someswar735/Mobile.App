3f3d1af8035e264bc6756ee75fc8a8120bb072b2
Someswar Sivakavi <120712448+someswar735@users.noreply.github.com>
2024-03-28 19:45:28 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,65 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
+import { getDatabase, ref, push, onValue } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+
+const appSettings = {
+    databaseURL: "https://playground-288de-default-rtdb.europe-west1.firebasedatabase.app/"
+}
+
+
+const app = initializeApp(appSettings)
+const database = getDatabase(app)
+const itemsINDB = ref(database, "items")
+
+
+const itemsEl = document.getElementById("items")
+
+onValue(itemsINDB, function(snapshot) {
+    let itemsArray = Object.values(snapshot.val())
+    
+    clearItemsListEl()
+    
+    // Challenge: Write a for loop where you console log each book.
+    for (let i = 0; i < itemsArray.length; i++) {
+        let currentItem = itemsArray[i]
+        
+        // Challenge: Use the appendBookToBooksListEl() function to append book instead of console logging
+        appendItemtoItemsListEl(currentItem)
+    }
+})
+
+function clearItemsListEl() {
+    itemsEl.innerHTML = ""
+}
+
+function appendItemtoItemsListEl(itemValue) {
+    itemsEl.innerHTML += `<li>${itemValue}</li>`
+}
+
+const inputFieldEl = document.getElementById("input-field")
+const addButtonEl = document.getElementById("add-button")
+
+const addToCartSound = new Audio("sound.mp3"); 
+const errorSound = new Audio("error.mp3");
+
+addButtonEl.addEventListener("click", function() {
+    let inputValue = inputFieldEl.value.trim(); 
+    if (inputValue === "") {
+        return; // Ignore empty inputs
+    }
+
+    // Check if  item already exists in the list
+    const existingItems = Object.values(itemsEl.children).map(item => item.textContent);
+    if (existingItems.includes(inputValue)) {
+        errorSound.play(); 
+        alert("Item already exists in the list!"); //error message
+        return;
+    }
+
+    // If item is not already in list, add it to the database and update
+    push(itemsINDB, inputValue);
+    inputFieldEl.value = ""; // Clear the input field
+
+    // sound effect
+    addToCartSound.play();
+});
+
